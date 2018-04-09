4f3c677f68d0f26d4998f1fd2d9a6dab6887891d
Brian Wright <briankw23@gmail.com>
2018-04-09 17:42:53 -0500

    got data in one place

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,9 @@ const eventTargetButton = (e) => {
     const buttonTarget = document.getElementById("start");
     buttonTarget.addEventListener("click", httpPlayer1);
     buttonTarget.addEventListener("click", httpPlayer2);
-
+    buttonTarget.addEventListener("click", httpPlayers);
+ 
+ 
 }
 function httpPlayer1 (e) {
     console.log(e);
@@ -63,7 +65,54 @@ printToDom = (domString, DivId) => {
     document.getElementById(DivId).innerHTML = domString;
 }
 
+
+
+function httpPlayers (e) {
+    let players = [];
+    const playerOneUser = document.getElementById("user1");
+    const playerOneUserValue = playerOneUser.value;
+    let http = new XMLHttpRequest();
+    http.onreadystatechange = function (){
+        if (http.readyState == 4 && http.status == 200) {
+            const data1 = JSON.parse(http.responseText);
+            console.log(data1);
+            players.push(data1.points.total);
+            players.push(data1.badges);
+            second();        
+        } else{
+            console.log("status", http.status);
+        }
+    }
+    http.open("GET", `https://teamtreehouse.com/${playerOneUserValue}.json`);
+    http.send();
+
+    function second (e) {
+        const playerTwoUser = document.getElementById("user2");
+        const playerTwoUserValue = playerTwoUser.value;
+            let http2 = new XMLHttpRequest();
+    http.onreadystatechange = function (){
+        if (http.readyState == 4 && http.status == 200) {
+            const data2 = JSON.parse(http.responseText);
+            console.log(data2);
+            players.push(data2.points.total);
+            players.push(data2.badges);
+        } else{
+            console.log("status", http.status);
+        }
+    }
+    http.open("GET", `https://teamtreehouse.com/${playerTwoUserValue}.json`);
+    http.send();
+    }
+    // console.log(players);
+    joiner(players);
+};
+
+const joiner = (playersArray) =>{
+    console.log(playersArray);
+    
+}
 const startApp = () => {
     eventTargetButton();
 }
 startApp();
+
