b59aac14dd3d1f2f1d7a40d9ae634bd014c9d0a9
Brian Wright <briankw23@gmail.com>
2018-04-10 16:17:01 -0500

    code cleanup

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,62 +1,21 @@
 const eventTargetButton = (e) => {
     const buttonTarget = document.getElementById("start");
-    // buttonTarget.addEventListener("click", httpPlayer1);
-    // buttonTarget.addEventListener("click", httpPlayer2);
     buttonTarget.addEventListener("click", httpPlayers);
 };
-// function httpPlayer1 (e) {
-//     console.log(e);
-//     const playerOneUser = document.getElementById("user1");
-//     const playerOneUserValue = playerOneUser.value;
-//     const http = new XMLHttpRequest();
-//     http.onreadystatechange = function (){
-//         if (http.readyState == 4 && http.status == 200) {
-//             const data1 = JSON.parse(http.responseText);
-//             console.log(data1);
-//             domString1(data1);
-//         } else{
-//             console.log("status", http.status);
-//         }
-//     }
-//     http.open("GET", `https://teamtreehouse.com/${playerOneUserValue}.json`);
-//     http.send();
-// };
-// function httpPlayer2 (e) {
-//     console.log(e);
-//     const playerTwoUser = document.getElementById("user2");
-//     const playerTwoUserValue = playerTwoUser.value;
-//     const http = new XMLHttpRequest();
-//     http.onreadystatechange = function (){
-//         if (http.readyState == 4 && http.status == 200) {
-//             const data2 = JSON.parse(http.responseText);
-//             console.log(data2);
-//             domString2(data2);
-//         } else{
-//             console.log("status", http.status);
-//         }
-//     }
-//     http.open("GET", `https://teamtreehouse.com/${playerTwoUserValue}.json`);
-//     http.send();
-// };
-
 const domString1 = (dataArray) =>{
-    console.log(dataArray);
     let domString ="";
     domString += `<h3>${dataArray.name}</h3>`;
     domString += `<img class="pic" src=${dataArray.gravatar_url}>`;
     domString +=`<h3 id="player1score">${dataArray.points.total}</h3>`;
     domString +=`<h3>Total Points</h3>`;
-    console.log(domString);
     printToDom(domString,"card1");
 };
 const domString2 = (dataArray) =>{
-    console.log(dataArray);
     let domString ="";
     domString += `<h3>${dataArray.name}</h3>`;
     domString += `<img class="pic" src=${dataArray.gravatar_url}>`;
     domString +=`<h3 id="player2score">${dataArray.points.total}</h3>`;
     domString +=`<h3>Total Points</h3>`;
-    console.log(domString);
     printToDom(domString, "card2");
 };
 printToDom = (domString, DivId) => {
@@ -70,7 +29,6 @@ function httpPlayers (e) {
     http.onreadystatechange = function (){
         if (http.readyState == 4 && http.status == 200) {
             const data1 = JSON.parse(http.responseText);
-            console.log(data1);
             players.push(data1);
             domString1(data1);
             second();        
@@ -88,10 +46,8 @@ function httpPlayers (e) {
     http.onreadystatechange = function (){
         if (http.readyState == 4 && http.status == 200) {
             const data2 = JSON.parse(http.responseText);
-            console.log(data2);
             players.push(data2);
             domString2(data2);
-            console.log(data2);
             joiner(players);
         } else{
             console.log("status", http.status);
@@ -99,19 +55,15 @@ function httpPlayers (e) {
     }
     http.open("GET", `https://teamtreehouse.com/${playerTwoUserValue}.json`);
     http.send();
-    }
-    // console.log(players);
-    
+    }   
 };
 const joiner = (playersArray) =>{
 
     let domString = "";
     if(playersArray[0].points.total > playersArray[1].points.total){
         domString += `<h3>${playersArray[0].name} is the winner!!!!</h3>`;
-        console.log(playersArray[0].badges);
         for (let i = 0; i < playersArray[0].badges.length; i++) {
-            domString += `<img class="img-responsive"  id="badge" src="${playersArray[0].badges[i].icon_url}">`;
-        }
+            domString += `<img class="img-responsive"  id="badge" src="${playersArray[0].badges[i].icon_url}">`;}
         }
          else
         {
